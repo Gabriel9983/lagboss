f91ec567814c1bc046a5e86bf4c9b5eba4b8babe
Gabriel9983 <202412288@gordoncollege.edu.ph>
2026-02-16 23:50:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const message = `As retreat season unfolds, hope the hush gets through to you like the everyday clamor never does. Let stillness sharpen your clarity, reflections ignite growth, and every moment edge you toward your heart's true peace.`;
+
+function showLetter() {
+  document.getElementById("introText").style.opacity = 0;
+  document.querySelector(".btn").style.display = "none";
+
+  setTimeout(() => {
+    const letterBox = document.getElementById("letterBox");
+    const typedText = document.getElementById("typedText");
+    letterBox.style.display = "block";
+    let i = 0;
+
+    function typeWriter() {
+      if (i < message.length) {
+        typedText.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 30);
+      }
+    }
+
+    typeWriter();
+  }, 600);
+}
